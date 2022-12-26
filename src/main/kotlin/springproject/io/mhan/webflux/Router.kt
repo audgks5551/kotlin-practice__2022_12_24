f436c4290e94e9620252a9a023b8890c0a8f6502
audgks5551 <audgks5551@daum.net>
2022-12-27 02:21:56 +0900

    spring webflux 함수형 엔드포인트

--- src/main/kotlin/springproject/io/mhan/webflux/Router.kt
+++ src/main/kotlin/springproject/io/mhan/webflux/Router.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions.route
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 
 @Configuration
 class Router {
@@ -15,4 +16,14 @@ class Router {
             .GET("/") { request ->  handler.sayHello(request) }
             // .GET("/", handler::sayHello) 위와 동일
             .build()
+
+    @Bean
+    fun userRouter(handler: UserHandler) : RouterFunction<ServerResponse> =
+        router {
+            "/users".nest {
+                GET("/{id}", handler::getUser)
+                GET("", handler::getAll)
+            }
+        }
+
 }
